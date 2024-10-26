bcf759e0c388349014bb46abeb636645290a05ae
Siraj617 <mohamedsirajily@gmail.com>
2024-10-26 18:50:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const helmet = require('helmet');
 const hpp = require('hpp');
 const xss = require('xss-clean');
 const csurf = require('csurf');
+const { Server } = require('socket.io');
+const socketHandler = require('./socket');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const limiter = require('./middlewares/rateLimiter');
 
@@ -37,8 +39,10 @@ app.use(xss());
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+
 }));
 
 app.use(cookieParser());
@@ -52,33 +56,47 @@ app.use(
         cookie: {
             secure: process.env.NODE_ENV === 'production',
             httpOnly: true,
-             maxAge: 7 * 24 * 60 * 60 * 1000,  // 1 day
+            maxAge: 7 * 24 * 60 * 60 * 1000,  // 1 week
         },
     })
 );
 
+// Logging middleware for session details
+app.use((req, res, next) => {
+    console.log("Session Detailssss:", req.session); // Log the session details
+    next(); // Call the next middleware or route handler
+});
+
 // CSRF protection middleware
 const csrfProtection = csurf({ cookie: true });
 app.use(csrfProtection);
-
 
 // Rate limiter middleware
 app.use('/api/', limiter);
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/', authRoutes);
 app.use('/api/protected', protectedRoutes);
 
 app.get('/api/csrf-token', (req, res) => {
     res.json({ csrfToken: req.csrfToken() });
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
 app.use(notFound);
 app.use(errorHandler);
+
+// const server = require('http').createServer(app);
+// const io = new Server(server, {
+//     cors: {
+//         origin: "http://localhost:3000", // Your frontend URL
+//         methods: ["GET", "POST", "DELETE", "PUT"],
+//         credentials: true,
+//     },
+// });
+
+// // Your socket handling code
+// socketHandler(io);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
