e0dedc0ac7c2737f72c2992ab688289f9995ca38
Siraj617 <mohamedsirajily@gmail.com>
2024-10-27 17:22:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(
         saveUninitialized: false,
         store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
         cookie: {
-            secure: true,
+            secure: false,
             httpOnly: true,
             sameSite: process.env.NODE_ENV === 'production' ? 'Lax' : 'Strict',
             maxAge: 7 * 24 * 60 * 60 * 1000,  // 1 week
